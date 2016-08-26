252594caba20273d76754981b3c658e6c649aeaa
14tondeu <maxime.tondeur@mines-paristech.fr>
2016-08-26 11:42:38 +0200

    change dimension of ic generated

--- mods/MAOOAM/ic_def.py
+++ mods/MAOOAM/ic_def.py
@@ -19,18 +19,15 @@ def load_IC() :
 		fichier.write("\n")
 		fichier.write("\n")
 		
-		fichier.write("X0=np.zeros("+str(ndim+1)+")")
+		fichier.write("X0=np.zeros("+str(ndim)+")")
 		fichier.write("\n")
 		fichier.write("\n")
 		
-		fichier.write("X0[0]=1. #one dimension more to have ndim+1 to support the multiplication with the tensor (j and k are in [|0,ndim|]")
-		fichier.write("\n")
-		fichier.write("\n")
 		
 		fichier.write("#psi variables")
 		fichier.write("\n")
 		for i in range(0,natm):
-			fichier.write("X0["+str(i+1)+"]=0.0 # typ="+str(awavenum[i].typ)+", NX0="+str(awavenum[i].Nx)+", Ny= "+str(awavenum[i].Ny))
+			fichier.write("X0["+str(i)+"]=0.0 # typ="+str(awavenum[i].typ)+", NX0="+str(awavenum[i].Nx)+", Ny= "+str(awavenum[i].Ny))
 			fichier.write("\n")
 
 		fichier.write("\n")
@@ -38,7 +35,7 @@ def load_IC() :
 		fichier.write("\n")
 		
 		for i in range(0,natm):
-			fichier.write("X0["+str(i+1+natm)+"]=0.0 # typ="+str(awavenum[i].typ)+", NX0="+str(awavenum[i].Nx)+", Ny= "+str(awavenum[i].Ny))
+			fichier.write("X0["+str(i+natm)+"]=0.0 # typ="+str(awavenum[i].typ)+", NX0="+str(awavenum[i].Nx)+", Ny= "+str(awavenum[i].Ny))
 			fichier.write("\n")
 			
 		fichier.write("\n")
@@ -46,7 +43,7 @@ def load_IC() :
 		fichier.write("\n")
 		
 		for i in range(0,noc):
-			fichier.write("X0["+str(i+1+natm+natm)+"]=0.0 # NX0="+str(owavenum[i].Nx)+", Ny= "+str(owavenum[i].Ny))
+			fichier.write("X0["+str(i+natm+natm)+"]=0.0 # NX0="+str(owavenum[i].Nx)+", Ny= "+str(owavenum[i].Ny))
 			fichier.write("\n")
 
 		fichier.write("\n")
@@ -54,7 +51,7 @@ def load_IC() :
 		fichier.write("\n")
 		
 		for i in range(0,noc):
-			fichier.write("X0["+str(i+1+natm+natm+noc)+"]=0.0 # NX0="+str(owavenum[i].Nx)+", Ny= "+str(owavenum[i].Ny))
+			fichier.write("X0["+str(i+natm+natm+noc)+"]=0.0 # NX0="+str(owavenum[i].Nx)+", Ny= "+str(owavenum[i].Ny))
 			fichier.write("\n")
 		fichier.close()
 

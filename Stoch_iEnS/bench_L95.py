30d7da094913acf0db8f654a93fc6ee7a79630c3
patricknraanes <patrick.n.raanes@gmail.com>
2018-11-15 14:07:04 +0100

    Better bench_L95.py

--- Stoch_iEnS/bench_L95.py
+++ Stoch_iEnS/bench_L95.py
@@ -6,18 +6,16 @@ sd0 = seed(3)
 # DA Configurations
 ##############################
 from mods.Lorenz95.sak08 import setup
-# setup.t.BurnIn = 5
 setup.t.T = 4**3.5
-setup.t.dkObs = 3
+setup.t.dkObs = 4
 
 # Get experiment control variable (CtrlVar) from arguments
 CtrlVar = sys.argv[1]
 # Set range of experimental settings
 if CtrlVar == 'N': # ens size
-  # xticks = [30]
-  xticks = [15, 17, 20, 25, 30, 40, 60, 200]
+  xticks = [13, 15, 17, 20, 25, 30, 40, 60, 100]
 
-xticks = array(xticks).repeat(3)
+xticks = array(xticks).repeat(16)
 
 # Parallelization and save-path setup
 xticks, save_path, iiRep = distribute(__file__,sys.argv,xticks,CtrlVar)
@@ -26,25 +24,21 @@ xticks, save_path, iiRep = distribute(__file__,sys.argv,xticks,CtrlVar)
 ##############################
 # Configs
 ##############################
-cfgs  = List_of_Configs()
+cfgs  = List_of_Configs(unique=True)
 
-# BASELINES
 cfgs += Climatology()
 cfgs += OptInterp()
 cfgs += Var3D()
 
-nIter  = 10
-Lag    = 3   # TODO: 8 is optimal ?
-infls  = [1.01, 1.02, 1.04, 1.06, 1.10, 1.2, 1.4]
-# infls  = [1.01, 1.03, 1.07, 1.25]
-
-for infl in infls:
-  cfgs +=   EnKF ('PertObs', N='?', infl=infl,)
-  for rot in [False, True]:
-    cfgs += EnKF ('Sqrt'   , N='?', infl=infl, rot=rot, )
-    cfgs += iEnKS('Sqrt'   , N='?', infl=infl, rot=rot, Lag=Lag, nIter=nIter)
-  cfgs +=   iEnKS('EnRML'  , N='?', infl=infl,          Lag=Lag, nIter=nIter)
-  cfgs +=   iEnKS('ES-MDA' , N='?', infl=infl,          Lag=Lag, nIter=nIter)
+for infl in [1.01, 1.02, 1.04, 1.06, 1.10, 1.16, 1.25, 1.4]:
+  for nIter in [3, 10]:
+    for Lag = [1, 2, 4]: # 
+      for rot in [False, True]:
+        cfgs +=  EnKF('PertObs', N='?', infl=infl,)
+        cfgs +=  EnKF('Sqrt'   , N='?', infl=infl, rot=rot, )
+        cfgs += iEnKS('Sqrt'   , N='?', infl=infl, rot=rot, Lag=Lag, nIter=nIter)
+        cfgs += iEnKS('EnRML'  , N='?', infl=infl,          Lag=Lag, nIter=nIter)
+        cfgs += iEnKS('ES-MDA' , N='?', infl=infl,          Lag=Lag, nIter=nIter)
 
 
 ##############################
@@ -70,7 +64,7 @@ for iX,(X,iR) in enumerate(zip(xticks,iiRep)):
     stat = C.assimilate(setup,xx,yy)
     avrg = stat.average_in_time()
 
-    stats[iX,0,iC] = stat
+    # stats[iX,0,iC] = stat
     avrgs[iX,0,iC] = avrg
   print_averages(cfgs, avrgs[iX,0],statkeys=['rmse_a','rmv_a','infl'])
 
@@ -90,8 +84,18 @@ np.savez(save_path,
 ##############################
 if 'WORKER' in sys.argv: sys.exit(0) # quit if script is running as worker.
 
-R = ResultsTable(save_path)
+# R = ResultsTable(save_path)
+
+# dkObs = 1
 # R = ResultsTable("data/Stoch_iEnS/bench_L95/P2720L/N_run2")
+
+# dkObs = 3
+# R = ResultsTable("data/Stoch_iEnS/bench_L95/P2720L/N_run3")
+# R          .load("data/Stoch_iEnS/bench_L95/ip-172-31-45-145/N_run1")
+# R.rm("rot:0")
+
+# dkObs = 4:
+
 
 # with coloring(): print("Averages over experiment repetition:")
 # R.print_mean_field('rmse_a',1,1,cols=slice(0,2))

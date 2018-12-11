e2ccfd4761edac10e14bcd62497b5fff1a405d14
patricknraanes <patrick.n.raanes@gmail.com>
2018-12-11 18:57:26 +0100

    Added 'unit tests'

---
+++ scripts/test_example_2.py
@@ -0,0 +1,138 @@
+# Just stupidly compare the full table.
+# Seed-dependent test.
+# ==> Test cannot be run with different seeds or computers.
+
+from common import *
+
+from tools.admin import _print_averages
+def pa(cfgs,avrgs): return _print_averages(cfgs,avrgs,statkeys=['rmse_a','rmse_f','rmse_u'])
+
+
+##############################
+# L63
+##############################
+from mods.Lorenz63.sak12 import setup
+setup.t.BurnIn=0
+setup.t.KObs=10
+sd0 = seed(9)
+
+# Cfgs
+cfgs  = List_of_Configs()
+cfgs += Climatology()
+cfgs += OptInterp()
+cfgs += Var3D(infl=0.9)
+cfgs += ExtKF(infl=90)
+cfgs += EnKF('Sqrt',    N=3 ,  infl=1.30)
+cfgs += EnKF('Sqrt',    N=10,  infl=1.02,rot=True)
+cfgs += EnKF('PertObs', N=500, infl=0.95,rot=False)
+cfgs += EnKF_N(         N=10,            rot=True)
+cfgs += iEnKS('Sqrt',   N=10,  infl=1.02,rot=True)
+cfgs += PartFilt(       N=100 ,reg=2.4  ,NER=0.3)
+cfgs += PartFilt(       N=800 ,reg=0.9  ,NER=0.2)
+cfgs += PartFilt(       N=4000,reg=0.7  ,NER=0.05)
+cfgs += PFxN(xN=1000,   N=30  ,Qs=2     ,NER=0.2)
+
+# Run
+xx,yy = simulate(setup)
+avrgs = []
+for ic,config in enumerate(cfgs):
+  config.store_u = True
+  seed(sd0+2)
+  stats = config.assimilate(setup,xx,yy)
+  avrgs += [ stats.average_in_time() ]
+
+table = pa(cfgs,avrgs)
+old = """
+      da_method       N  upd_a     infl  rot     NER  Qs  reg    xN  |  rmse_a ±    rmse_f ±    rmse_u ±
+----  -----------  ----  -------  -----  -----  ----  --  ---  ----  -  ----------  ----------  ----------
+[0]   Climatology                                                    |   8.265 1     8.265 1      7.93 3
+[1]   OptInterp                                                      |   1.265 0.2   8.265 1     1.325 0.1
+[2]   Var3D                        0.9                               |   1.212 0.2   2.569 1     1.584 0.3
+[3]   ExtKF                       90                                 |  0.9001 0.2    1.86 0.8   1.152 0.2
+[4]   EnKF            3  Sqrt      1.3                               |  0.8368 0.2   1.514 0.5  0.9236 0.1
+[5]   EnKF           10  Sqrt      1.02  True                        |  0.7257 0.3     1.3 0.3  0.8452 0.2
+[6]   EnKF          500  PertObs   0.95  False                       |   0.713 0.3   1.289 0.3  0.8216 0.2
+[7]   EnKF_N         10                  True                        |  0.7471 0.2   1.317 0.3  0.8661 0.2
+[8]   iEnKS          10  Sqrt      1.02  True                        |  0.8153 0.3  0.9145 0.3  0.4997 0.3
+[9]   PartFilt      100                         0.3       2.4        |  0.6182 0.2   1.401 0.7  0.8489 0.1
+[10]  PartFilt      800                         0.2       0.9        |  0.4684 0.2  0.9491 0.4   0.587 0.1
+[11]  PartFilt     4000                         0.05      0.7        |  0.4317 0.1   0.815 0.3  0.5209 0.1
+[12]  PFxN           30                         0.2    2       1000  |   0.862 0.2   1.809 0.5   1.155 0.2
+"""[1:-1]
+
+def test_len():
+  assert len(old)==len(table)
+
+table = table.split('\n')
+old   = old  .split('\n')
+
+import pytest
+@pytest.mark.parametrize(('lineno'),arange(len(table)))
+def test_tables(lineno):
+    assert table[lineno] == old[lineno]
+
+
+
+##############################
+# L95
+##############################
+from mods.Lorenz95.sak08 import setup
+setup.t.BurnIn=0
+setup.t.KObs=10
+sd0 = seed(9)
+
+# Cfgs
+cfgs  = List_of_Configs()
+cfgs += Climatology()
+cfgs += OptInterp()
+cfgs += Var3D(infl=1.05)
+cfgs += ExtKF(infl=6)
+cfgs += EnKF('PertObs'        ,N=40,infl=1.06)
+cfgs += EnKF('Sqrt'           ,N=28,infl=1.02,rot=True)
+
+cfgs += EnKF_N(N=24,rot=True)
+cfgs += EnKF_N(N=24,rot=True,xN=2)
+cfgs += iEnKS('Sqrt',N=40,infl=1.01,rot=True)
+
+cfgs += LETKF(         N=7,rot=True,infl=1.04,loc_rad=4)
+cfgs += SL_EAKF(       N=7,rot=True,infl=1.07,loc_rad=6)
+
+
+# Run
+xx,yy = simulate(setup)
+avrgs = []
+for ic,config in enumerate(cfgs):
+  config.store_u = True
+  seed(sd0+2)
+  stats = config.assimilate(setup,xx,yy)
+  avrgs += [ stats.average_in_time() ]
+
+table = pa(cfgs,avrgs)
+old = """
+      da_method     N  upd_a    infl  rot   loc_rad  xN  |   rmse_a ±        rmse_f ±        rmse_u ±
+----  -----------  --  -------  ----  ----  -------  --  -  --------------  --------------  --------------
+[0]   Climatology                                        |   0.8347 0.2      0.8347 0.2      0.8347 0.2
+[1]   OptInterp                                          |   0.1275 0.03     0.8347 0.2      0.1275 0.03
+[2]   Var3D                     1.05                     |  0.08993 0.04     0.2405 0.2     0.08993 0.04
+[3]   ExtKF                     6                        |  0.02986 0.0008  0.02987 0.0009  0.02986 0.0008
+[4]   EnKF         40  PertObs  1.06                     |  0.03031 0.001   0.03018 0.0009  0.03031 0.001
+[5]   EnKF         28  Sqrt     1.02  True               |     0.03 0.001   0.02991 0.001      0.03 0.001
+[6]   EnKF_N       24                 True               |   0.0304 0.001   0.03032 0.001    0.0304 0.001
+[7]   EnKF_N       24                 True            2  |  0.03039 0.001   0.03031 0.001   0.03039 0.001
+[8]   iEnKS        40  Sqrt     1.01  True               |  0.03017 0.001   0.03011 0.0009  0.03017 0.001
+[9]   LETKF         7           1.04  True        4      |  0.03027 0.001   0.03022 0.001   0.03027 0.001
+[10]  SL_EAKF       7           1.07  True        6      |  0.03067 0.001    0.0305 0.001   0.03067 0.001
+"""[1:-1]
+
+table = table.split('\n')
+old   = old  .split('\n')
+
+def test_len():
+  assert len(old)==len(table)
+
+import pytest
+@pytest.mark.parametrize(('lineno'),arange(len(table)))
+def test_tables(lineno):
+    assert table[lineno] == old[lineno]
+
+
--- scripts/test_iEnKS.py
+++ scripts/test_iEnKS.py
@@ -1,6 +1,9 @@
+# Tests with the LA model, which are very useful for testing
+# boundary cases of the iEnKS (e.g. nIter=1, Lag=0).
+
 from common import *
 
-sd0 = seed_init(5)
+sd0 = seed_init(3)
 
 cfgs  = List_of_Configs(unique=True)
 
@@ -41,100 +44,219 @@ for ic,config in enumerate(cfgs):
   stats += [ config.assimilate(setup,xx,yy) ]
   avrgs += [ stats[ic].average_in_time() ]
 
-print_averages(cfgs,avrgs,statkeys=['rmse_a','rmse_f','rmse_u'])
+
+##############################
+# Test aux functions
+##############################
+inds = cfgs.inds # lookup inds
+
+def allsame(xx): 
+  return np.allclose(xx, xx[0], atol=1e-10, rtol=0) # rtol=0 => only atol matters.
+
+def gr(ii,f_a_u='a'): # grab_rmses
+  return [avrgs[i]['rmse_'+f_a_u].val for i in ii]
 
 
-# Example that can be used (after changes) with diff to unit-test developments.
-# >>> sd0 = seed_init(5)
-# >>> 
-# >>> cfgs  = List_of_Configs(unique=True)
-# >>> 
-# >>> from mods.LA.small import setup
-# >>> setup.t.BurnIn=0
-# >>> setup.t.KObs=10
-# >>> 
-# >>> cfgs +=  EnKF('Sqrt'        , N=20,                      )
-# >>> cfgs +=  EnKF('PertObs'     , N=20,                      )
-# >>> cfgs +=  EnKF('DEnKF'       , N=20,                      )
-# >>> for Lag in [0,1,3]:
-# >>>   cfgs +=  EnKS('Sqrt'      , N=20, Lag=Lag,             )
-# >>>   cfgs +=  EnKS('PertObs'   , N=20, Lag=Lag,             )
-# >>>   cfgs +=  EnKS('DEnKF'     , N=20, Lag=Lag,             )
-# >>>   for nIter in [1,4]:
-# >>>     for MDA in [False,True]:
-# >>>       cfgs += iEnKS('Sqrt'    , N=20, Lag=Lag, nIter=nIter, MDA=MDA)
-# >>>       cfgs += iEnKS('PertObs' , N=20, Lag=Lag, nIter=nIter, MDA=MDA)
-# >>>       cfgs += iEnKS('Order1'  , N=20, Lag=Lag, nIter=nIter, MDA=MDA)
-# 
-# Yielded output:
-#       da_method  upd_a    Lag  MDA    nIter  |  rmse_a ±     rmse_f ±      rmse_u ±
-# ----  ---------  -------  ---  -----  -----  -  -----------  -----------  -------------
-# [0]   EnKF       Sqrt                        |  0.1294 0.06   0.174 0.07   0.1651 0.09
-# [1]   EnKF       PertObs                     |  0.1375 0.07  0.1814 0.08   0.1726 0.09
-# [2]   EnKF       DEnKF                       |  0.1614 0.08  0.2049 0.09   0.1962 0.1
-# [3]   EnKS       Sqrt       0                |  0.1294 0.06   0.174 0.07   0.1651 0.09
-# [4]   EnKS       PertObs    0                |  0.1375 0.07  0.1814 0.08   0.1726 0.09
-# [5]   EnKS       DEnKF      0                |  0.1614 0.08  0.2049 0.09   0.1962 0.1
-# [6]   iEnKS      Sqrt       0  False      1  |  0.1294 0.06   0.174 0.07   0.1651 0.09
-# [7]   iEnKS      PertObs    0  False      1  |  0.1375 0.07  0.1814 0.08   0.1726 0.09
-# [8]   iEnKS      Order1     0  False      1  |  0.1614 0.08  0.2049 0.09   0.1962 0.1
-# [9]   iEnKS      Sqrt       0  True       1  |  0.1294 0.06   0.174 0.07   0.1651 0.09
-# [10]  iEnKS      PertObs    0  True       1  |  0.1375 0.07  0.1814 0.08   0.1726 0.09
-# [11]  iEnKS      Order1     0  True       1  |  0.1614 0.08  0.2049 0.09   0.1962 0.1
-# [12]  iEnKS      Sqrt       0  False      4  |  0.1294 0.06   0.174 0.07   0.1651 0.09
-# [13]  iEnKS      PertObs    0  False      4  |  0.1375 0.07  0.1814 0.08   0.1726 0.09
-# [14]  iEnKS      Order1     0  False      4  |  0.1309 0.06  0.1749 0.06   0.1661 0.09
-# [15]  iEnKS      Sqrt       0  True       4  |  0.1294 0.06   0.174 0.07   0.1651 0.09
-# [16]  iEnKS      PertObs    0  True       4  |   0.188 0.04  0.2269 0.07   0.2191 0.08
-# [17]  iEnKS      Order1     0  True       4  |  0.1318 0.07  0.1764 0.08   0.1674 0.09
-# [18]  EnKS       Sqrt       1                |  0.1294 0.06   0.174 0.07   0.1213 0.04
-# [19]  EnKS       PertObs    1                |  0.1375 0.07  0.1814 0.08   0.1295 0.04
-# [20]  EnKS       DEnKF      1                |  0.1614 0.08  0.2049 0.09   0.1534 0.06
-# [21]  iEnKS      Sqrt       1  False      1  |  0.1294 0.06   0.174 0.07   0.1213 0.04
-# [22]  iEnKS      PertObs    1  False      1  |  0.1375 0.07  0.1814 0.08   0.1295 0.04
-# [23]  iEnKS      Order1     1  False      1  |  0.1614 0.08  0.2049 0.09   0.1534 0.06
-# [24]  iEnKS      Sqrt       1  True       1  |  0.1294 0.06   0.174 0.07   0.1213 0.04
-# [25]  iEnKS      PertObs    1  True       1  |  0.1375 0.07  0.1814 0.08   0.1295 0.04
-# [26]  iEnKS      Order1     1  True       1  |  0.1614 0.08  0.2049 0.09   0.1534 0.06
-# [27]  iEnKS      Sqrt       1  False      4  |  0.1294 0.06   0.174 0.07   0.1213 0.04
-# [28]  iEnKS      PertObs    1  False      4  |  0.1375 0.07  0.1814 0.08   0.1295 0.04
-# [29]  iEnKS      Order1     1  False      4  |  0.1309 0.06  0.1749 0.06   0.1229 0.04
-# [30]  iEnKS      Sqrt       1  True       4  |  0.1294 0.06   0.174 0.07   0.1213 0.04
-# [31]  iEnKS      PertObs    1  True       4  |   0.188 0.04  0.2269 0.07   0.1807 0.04
-# [32]  iEnKS      Order1     1  True       4  |  0.1318 0.07  0.1764 0.08   0.1235 0.04
-# [33]  EnKS       Sqrt       3                |  0.1294 0.06   0.174 0.07   0.0727 0.01
-# [34]  EnKS       PertObs    3                |  0.1375 0.07  0.1814 0.08  0.08216 0.01
-# [35]  EnKS       DEnKF      3                |  0.1614 0.08  0.2049 0.09  0.09731 0.02
-# [36]  iEnKS      Sqrt       3  False      1  |  0.1294 0.06   0.174 0.07   0.0727 0.01
-# [37]  iEnKS      PertObs    3  False      1  |  0.1375 0.07  0.1814 0.08  0.08216 0.01
-# [38]  iEnKS      Order1     3  False      1  |  0.1614 0.08  0.2049 0.09  0.09731 0.02
-# [39]  iEnKS      Sqrt       3  True       1  |  0.1294 0.06   0.174 0.07   0.0727 0.01
-# [40]  iEnKS      PertObs    3  True       1  |  0.1375 0.07  0.1814 0.08  0.08216 0.01
-# [41]  iEnKS      Order1     3  True       1  |  0.1614 0.08  0.2049 0.09  0.09731 0.02
-# [42]  iEnKS      Sqrt       3  False      4  |  0.1294 0.06   0.174 0.07   0.0727 0.01
-# [43]  iEnKS      PertObs    3  False      4  |  0.1375 0.07  0.1814 0.08  0.08216 0.01
-# [44]  iEnKS      Order1     3  False      4  |  0.1309 0.06  0.1749 0.06  0.07585 0.009
-# [45]  iEnKS      Sqrt       3  True       4  |  0.1294 0.06   0.174 0.07   0.0727 0.01
-# [46]  iEnKS      PertObs    3  True       4  |   0.188 0.04  0.2269 0.07   0.1378 0.01
-# [47]  iEnKS      Order1     3  True       4  |  0.1318 0.07  0.1764 0.08  0.07394 0.01
+##############################
+# Tests
+##############################
+# These don't evaluate the actual numbers, just that they match with the results from other
+# methods that are supposed to be equivalent.
+# * Understanding why the matches should (and shouldn't) arise is very instructive.
+# * Tests can be run with any initial seed.
 
-# For proper unit-testing, this should of course be made seed-independent.
-# This requires testing (instead of the absolute values, as with the above)
-# which values match, and that these values also match after the changes you've made. 
+# For Sqrt flavour:
+# - All filter/analysis (f/a) stats should be equal for all EnKF/EnKS/iEnKS(nIter/Lag/MDA).
+ii = inds(strict=1,upd_a='Sqrt')
+def test_EnKF_Sqrt():                                      assert allsame(gr(ii,'a')) and allsame(gr(ii,'f'))
+# - However, the smoother stats (u) depend on the Lag, of course:
+ii = inds(strict=0,upd_a='Sqrt',Lag=0) # Also has the non-iter filters
+def test_EnKF_Sqrt_u():                                    assert allsame(gr(ii,'u'))
+ii = inds(strict=1,upd_a='Sqrt',Lag=1) # Also has the non-iter smoothers
+def test_EnKF_Sqrt_Lag1_u():                               assert allsame(gr(ii,'u'))
+ii = inds(strict=1,upd_a='Sqrt',Lag=3) # Also has the non-iter smoothers
+def test_EnKF_Sqrt_Lag3_u():                               assert allsame(gr(ii,'u'))
 
-# The values that match should be interpreted. For example, in the linear case (LA):
-#  - Non-iterative methods yield equal f/a stats as iterative methods,
-#    and this is independent of the Lag used.
-#    An exception to the above rule is that nIter affects the f/a stats for
-#    * PertObs with         MDA
-#    * Order1   with/without MDA
-#    though they remain equal to non-iterative methods for nIter==1.
-# NB: For iEnKS PertObs (not MDA) to be independent of Lag and nIter,
-#     and reproduce the non-iterative filter, one must ensure that
-#     the perturbations are drawn in the same order, and use the same sign.
-#  - EnKS and iEnKS yield the same 'u' stats.
-#  - If Lag==0, then this also equals the 'u' stats of EnKF (excpet with Order1).
-#
+# For PertObs flavour::
+# - f/a stats all equal except for MDA with nIter>1.
+ii = inds(strict=0,upd_a='PertObs',MDA=0) +\
+     inds(strict=1,upd_a='PertObs',MDA=1,nIter=1)
+def test_EnKF_PertObs():                                   assert allsame(gr(ii,'a')) and allsame(gr(ii,'f'))
+# - Still with nIter=4, f/a stats of MDA does not depend on Lag.
+ii = inds(strict=1,upd_a='PertObs',nIter=4,MDA=1)
+def test_EnKF_PertObs_nIter4():                            assert allsame(gr(ii,'a')) and allsame(gr(ii,'f'))
+# - u stats equal for filter and (iter/non-iter) smoothers with Lag=0, except MDA with nIter>1:
+ii = inds(strict=0,upd_a='PertObs',MDA=0,Lag=0) +\
+     inds(strict=1,upd_a='PertObs',MDA=1,Lag=0,nIter=1)
+def test_EnKF_PertObs_u():                                 assert allsame(gr(ii,'u'))
+# - u stats equal for            (iter/non-iter) smoothers with Lag=1, except MDA with nIter>1:
+ii =      inds(upd_a='PertObs',Lag=1)
+ii.remove(inds(upd_a='PertObs',Lag=1,MDA=1,nIter=4)[0])
+def test_EnKF_PertObs_Lag1_u():                            assert allsame(gr(ii,'u'))
+# - u stats equal for            (iter/non-iter) smoothers with Lag=3, except MDA with nIter>1:
+ii =      inds(upd_a='PertObs',Lag=3)
+ii.remove(inds(upd_a='PertObs',Lag=3,MDA=1,nIter=4)[0])
+def test_EnKF_PertObs_Lag3_u():                            assert allsame(gr(ii,'u'))
+
+# For Order1 (DEnKF) flavour:
+# - f/a stats all equal except for nIter>1:
+ii = inds(upd_a='DEnKF') +\
+     inds(upd_a='Order1',nIter=1)
+def test_EnKF_Order1():                                    assert allsame(gr(ii,'a')) and allsame(gr(ii,'f'))
+# - f/a stats independent of Lag for non-MDA and a given nIter:
+ii = inds(upd_a='Order1',nIter=4,MDA=0)
+def test_EnKF_Order1_nIter4_MDA0():                        assert allsame(gr(ii,'a')) and allsame(gr(ii,'f'))
+# - f/a stats independent of Lag for     MDA and a given nIter:
+ii = inds(upd_a='Order1',nIter=4,MDA=1)
+def test_EnKF_Order1_nIter4_MDA1():                        assert allsame(gr(ii,'a')) and allsame(gr(ii,'f'))
+# - u   stats equal for EnKS/iEnKS(nIter=1) for a given Lag:
+ii = inds(strict=0,upd_a='DEnKF' ,Lag=0) +\
+     inds(         upd_a='Order1',Lag=0,nIter=1)
+def test_EnKF_Order1_nIter1_Lag0_u():                      assert allsame(gr(ii,'u'))
+ii = inds(da=EnKS, upd_a='DEnKF' ,Lag=1) +\
+     inds(         upd_a='Order1',Lag=1,nIter=1)
+def test_EnKF_Order1_nIter1_Lag1_u():                      assert allsame(gr(ii,'u'))
+ii = inds(da=EnKS, upd_a='DEnKF' ,Lag=3) +\
+     inds(         upd_a='Order1',Lag=3,nIter=1)
+def test_EnKF_Order1_nIter1_Lag3_u():                      assert allsame(gr(ii,'u'))
+
+# A table perspective of the tests:
+# Each benchmark result has been replaced by a letter,
+# but the letter is repeated for equal results.
+# I.e. the tests check that the letters occur everywhere they should.
+# The 'X#' stand for results that do not repeat anywhere.
+
+#       da_method  upd_a    Lag  MDA    nIter  |  rmse_a  rmse_f   rmse_u
+# ----  ---------  -------  ---  -----  -----  -  ------  ------  --------
+# [0]   EnKF       Sqrt                        |       A       B        C
+# [1]   EnKF       PertObs                     |       F       G        J
+# [2]   EnKF       DEnKF                       |       M       N        S
+
+
+# [3]   EnKS       Sqrt       0                |       A       B        C
+# [4]   EnKS       PertObs    0                |       F       G        J
+# [5]   EnKS       DEnKF      0                |       M       N        S
+
+# [6]   iEnKS      Sqrt       0  False      1  |       A       B        C
+# [7]   iEnKS      PertObs    0  False      1  |       F       G        J
+# [8]   iEnKS      Order1     0  False      1  |       M       N        S
+# [9]   iEnKS      Sqrt       0  True       1  |       A       B        C
+# [10]  iEnKS      PertObs    0  True       1  |       F       G        J
+# [11]  iEnKS      Order1     0  True       1  |       M       N        S
+
+# [12]  iEnKS      Sqrt       0  False      4  |       A       B        C
+# [13]  iEnKS      PertObs    0  False      4  |       F       G        J
+# [14]  iEnKS      Order1     0  False      4  |       O       P       X4
+# [15]  iEnKS      Sqrt       0  True       4  |       A       B        C
+# [16]  iEnKS      PertObs    0  True       4  |       H       I       X1
+# [17]  iEnKS      Order1     0  True       4  |       Q       R       X5
+
+
+# [18]  EnKS       Sqrt       1                |       A       B        D
+# [19]  EnKS       PertObs    1                |       F       G        K
+# [20]  EnKS       DEnKF      1                |       M       N        T
+
+# [21]  iEnKS      Sqrt       1  False      1  |       A       B        D
+# [22]  iEnKS      PertObs    1  False      1  |       F       G        K
+# [23]  iEnKS      Order1     1  False      1  |       M       N        T
+# [24]  iEnKS      Sqrt       1  True       1  |       A       B        D
+# [25]  iEnKS      PertObs    1  True       1  |       F       G        K
+# [26]  iEnKS      Order1     1  True       1  |       M       N        T
+
+# [27]  iEnKS      Sqrt       1  False      4  |       A       B        D
+# [28]  iEnKS      PertObs    1  False      4  |       F       G        K
+# [29]  iEnKS      Order1     1  False      4  |       O       P       X6
+# [30]  iEnKS      Sqrt       1  True       4  |       A       B        D
+# [31]  iEnKS      PertObs    1  True       4  |       H       I       X2
+# [32]  iEnKS      Order1     1  True       4  |       Q       R       X7
+
+
+# [33]  EnKS       Sqrt       3                |       A       B        E
+# [34]  EnKS       PertObs    3                |       F       G        L
+# [35]  EnKS       DEnKF      3                |       M       N        U
+
+# [36]  iEnKS      Sqrt       3  False      1  |       A       B        E
+# [37]  iEnKS      PertObs    3  False      1  |       F       G        L
+# [38]  iEnKS      Order1     3  False      1  |       M       N        U
+# [39]  iEnKS      Sqrt       3  True       1  |       A       B        E
+# [40]  iEnKS      PertObs    3  True       1  |       F       G        L
+# [41]  iEnKS      Order1     3  True       1  |       M       N        U
+
+# [42]  iEnKS      Sqrt       3  False      4  |       A       B        E
+# [43]  iEnKS      PertObs    3  False      4  |       F       G        L
+# [44]  iEnKS      Order1     3  False      4  |       O       P       X8
+# [45]  iEnKS      Sqrt       3  True       4  |       A       B        E
+# [46]  iEnKS      PertObs    3  True       4  |       H       I       X3
+# [47]  iEnKS      Order1     3  True       4  |       Q       R       X9
+
+
+##############################
+# Seed-dependent test
+##############################
+# Just stupidly compare the full table.
+# ==> Test cannot be run with different seeds or computers.
+# Seed used: sd0 = seed_init(3). Test run on my Mac.
+
+def test_a():
+  assert gr(arange(len(cfgs)),'a') == \
+      [0.11696645982504372 , 0.13707255797444515 , 0.16013057281282431 ,
+       0.11696645982504382 , 0.13707255797444512 , 0.16013057281282431 ,
+       0.11696645982504426 , 0.13707255797444598 , 0.16013057281282492 ,
+       0.11696645982504426 , 0.13707255797444598 , 0.16013057281282492 ,
+       0.1169664598250439  , 0.13707255797444512 , 0.1186316809275947  ,
+       0.11696645982504399 , 0.15353644282849052 , 0.11857822272148509 ,
+       0.11696645982504382 , 0.13707255797444512 , 0.16013057281282431 ,
+       0.11696645982504422 , 0.13707255797444584 , 0.16013057281282467 ,
+       0.11696645982504422 , 0.13707255797444584 , 0.16013057281282467 ,
+       0.1169664598250439  , 0.13707255797444509 , 0.1186316809275947  ,
+       0.116966459825044   , 0.15353644282849072 , 0.11857822272148516 ,
+       0.11696645982504382 , 0.13707255797444512 , 0.16013057281282431 ,
+       0.11696645982504425 , 0.13707255797444587 , 0.16013057281282467 ,
+       0.11696645982504425 , 0.13707255797444587 , 0.16013057281282467 ,
+       0.11696645982504389 , 0.13707255797444509 , 0.11863168092759468 ,
+       0.11696645982504399 , 0.15353644282849077 , 0.11857822272148516 ]
+
+def test_f():
+  assert gr(arange(len(cfgs)),'f') == \
+      [0.19684477973528794 , 0.21429229233638106 , 0.23934387451007286 ,
+       0.19684477973528802 , 0.214292292336381   , 0.23934387451007291 ,
+       0.19684477973528852 , 0.21429229233638189 , 0.23934387451007347 ,
+       0.19684477973528852 , 0.21429229233638189 , 0.23934387451007347 ,
+       0.19684477973528816 , 0.21429229233638106 , 0.19828766346772814 ,
+       0.19684477973528819 , 0.23049048795386004 , 0.19858885018530187 ,
+       0.19684477973528802 , 0.214292292336381   , 0.23934387451007286 ,
+       0.19684477973528844 , 0.21429229233638178 , 0.23934387451007322 ,
+       0.19684477973528844 , 0.21429229233638178 , 0.23934387451007322 ,
+       0.19684477973528811 , 0.214292292336381   , 0.19828766346772814 ,
+       0.19684477973528824 , 0.23049048795386023 , 0.19858885018530195 ,
+       0.19684477973528802 , 0.214292292336381   , 0.23934387451007286 ,
+       0.19684477973528847 , 0.21429229233638178 , 0.23934387451007322 ,
+       0.19684477973528847 , 0.21429229233638178 , 0.23934387451007322 ,
+       0.19684477973528811 , 0.214292292336381   , 0.19828766346772814 ,
+       0.19684477973528819 , 0.23049048795386026 , 0.19858885018530195 ]
+
+def test_u():
+  assert gr(arange(len(cfgs)),'u') == \
+      [0.18086911575323916  , 0.19884834546399385  , 0.22350121417062313  ,
+       0.18086911575323922  , 0.19884834546399388  , 0.22350121417062319  ,
+       0.18086911575323966  , 0.19884834546399466  , 0.22350121417062382  ,
+       0.18086911575323966  , 0.19884834546399466  , 0.22350121417062382  ,
+       0.18086911575323933  , 0.1988483454639938   , 0.18235646695970145  ,
+       0.18086911575323938  , 0.21509967892878615  , 0.18258672469253867  ,
+       0.10809005405428855  , 0.12872786931335145  , 0.15138717068466812  ,
+       0.10809005405428891  , 0.12872786931335212  , 0.15138717068466845  ,
+       0.10809005405428891  , 0.12872786931335212  , 0.15138717068466845  ,
+       0.10809005405428862  , 0.12872786931335142  , 0.10979974263086155  ,
+       0.10809005405428863  , 0.14479974441156154  , 0.10969822797890079  ,
+       0.05884180343829841  , 0.087850672189102169 , 0.083764782614186017 ,
+       0.058841803438298215 , 0.08785067218910271  , 0.083764782614185893 ,
+       0.058841803438298215 , 0.08785067218910271  , 0.083764782614185893 ,
+       0.058841803438298222 , 0.087850672189102114 , 0.061641461112443896 ,
+       0.058841803438298201 , 0.096727617207417904 , 0.059231682718777934 ]
+
+
+
+
 # For nonlinear dynamics, the (non-iterative) EnKF (f/a/u stats)
 # are reproduced by the iEnKS with Lag=0 (and nIter==1 if h is also nonlin).
 # However, the 'u' stats of the non-iterative EnKS(Lag>0) are not reproduced.
